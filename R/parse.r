153b57a2902bc9a7a8d02dda0892654bda7add51
Wei-Chen Chen <wccsnow@gmail.com>
2013-09-29 18:17:10 +0000

    version 0.2-0

--- R/parse.r
+++ R/parse.r
@@ -1,14 +1,18 @@
 ### Internal parsing methods
 
-parse.prof <- function(x, ...)
-{
+parse.prof <- function(x, ...){
   UseMethod("parse.prof")
 }
 
 
-# fpmpi
-parse.prof.fpmpi <- function(x, ...)
-{
+### default method (Don't delete/change this.)
+parse.prof.default <- function(x, ...){
+  stop("Profiler type is not found.")
+}
+
+
+### fpmpi
+parse.prof.fpmpi <- function(x, ...){
   # For return
   ret <- list()
   
@@ -74,6 +78,43 @@ parse.prof.fpmpi <- function(x, ...)
   ret <- parsed_fpmpi_2_df(ret)
   
   return( ret )
-}
+} # End of parse.prof.fpmpi().
 
+
+### mpiP
+parse.prof.mpip <- function(x, ...){
+  #Rscript for profiling mpiP
+  
+  ret_mpip <- list()
+
+  #lines <- readLines("12-null-null.exe.1.24393.1.mpiP")
+  lines <- x 
+  #selection the region between ---- and --- putting it in time series space using embed
+  regions <- t(t(embed(grep("@---", lines), 2)) + c(-2, 2)) 
+  #mapply on set of regions
+  ret_mpip <- mapply(function(start,stop) {
+    #converting to character without having to worry about spaces and empty lines
+    chunk <- paste(lines[start:stop], collapse = "\n")
+    #resusing the chunk
+    chunk <- gsub("Line Parent_Funct", "Line_Parent_Funct", chunk)
+    #making a text file connection to read it as table since it follows pattern
+    tc <- textConnection(chunk)
+    #reading as table
+    df <- read.table(tc, header = T)
+    #closing connection
+    close(tc)
+    #returning the result
+    ret_mpip <- df
+    return(ret_mpip)
+    #arguments of mapply
+  }, regions[,2], regions[,1])
+
+  return(ret_mpip)
+} # End of parse.prof.mpip().
+
+
+### tau
+parse.prof.tau <- function(x, ...){
+  stop("parse.prof for TAU is not implemented yet.")
+} # End of parse.prof.tau().
 
